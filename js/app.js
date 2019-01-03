be2140ff6b521fe64c3b43a1a1f3498f51aeb2b6
Nicholas Humes <njhumes@gmail.com>
2019-01-03 09:35:09 -0700

    working button functionality

--- js/app.js
+++ js/app.js
@@ -9,23 +9,24 @@ class Pet {
         this.sleepiness = 4;
         this.age = 1;
     }
-    feed() {
+    feed(){
+        console.log(this);
         this.hunger -= 3;
+        console.log(typeof this.hunger);
         $('#hunger-level').text(`Hunger: ${this.hunger}/10`);
         console.log(this.hunger)
         $('.gif').remove()
         $('.game-display').append('<img class="gif" src="https://i.pinimg.com/originals/b5/49/c5/b549c552687942777915f92519d5054c.gif">')
     }
-    play() {
+    play(){
         this.boredom -= 4;
         $('#boredom-level').text(`Boredom: ${this.boredom}/10`);
         console.log(this.boredom)
         console.log(typeof this.boredom)
-
         $('.gif').remove()
         $('.game-display').append('<img class="gif" src="https://i.gifer.com/origin/7d/7d6dd7aee61f0eae8caa330e54226a67_w200.gif">')
     }
-    sleep() {
+    sleep(){
         this.sleepiness -= 4;
         $('#sleep-level').text(`Sleepiness: ${this.sleepiness}/10`);
         $('.gif').remove()
@@ -33,7 +34,7 @@ class Pet {
     }
 }
 
-let bart = new Pet('bart');
+const bart = new Pet('bart');
 
 
 function makeGame(){
@@ -93,53 +94,19 @@ function makeGame(){
 $('.start-button').click(makeGame);
 
 // add functionality to game buttons
-$('.game').on('click', '#feed', bart.feed);
-$('.game').on('click', '#play', bart.play);
-$('.game').on('click', '#rest', bart.sleep);
+$('.game').on('click', '#feed', function(){
+    bart.feed();
+})
+$('.game').on('click', '#play', function(){
+    bart.play();
+})
+$('.game').on('click', '#rest', function(){
+    bart.sleep();
+})
 
-console.log(game.time)
+// 1. Not let the numbers go negative
+// 2. Have a game over message screen and give the option to restart
+// 3. Add progress bars to the levels
+// Bonus: morph after age 10
 
 
-
-
-
-
-
-// function increaseTime() {
-//     game.time += 1;
-//     increaseLevels();
-//     stopTimer();
-// }
-// function stopTimer() {
-//     if(bart.hunger > 9){
-//         clearInterval(timeRunning);
-//         console.log('game over');
-//     }
-//     if(bart.boredom > 9){
-//         clearInterval(timeRunning);
-//         console.log('game over')
-//     }
-//     if (bart.sleepiness > 9) {
-//         clearInterval(timeRunning);
-//         console.log('game over')
-//     }
-// }
-
-// function increaseLevels(){
-//     if(game.time % 2 === 0){
-//         bart.hunger += 1;
-//         $('#hunger-level').text(`Hunger: ${bart.hunger}/10`);
-//     }
-//     if(game.time % 3 === 0){
-//         bart.boredom += 2;
-//         $('#boredom-level').text(`Boredom: ${bart.boredom}/10`);
-//     }
-//     if(game.time % 5 === 0){
-//         bart.sleepiness += 3;
-//         $('#sleep-level').text(`Sleepiness: ${bart.sleepiness}/10`);
-//     }
-//     if(game.time % 15 === 0){
-//         bart.age += 1;
-//         $('#age-level').text(`Age: ${bart.age}`);
-//     }
-// }
