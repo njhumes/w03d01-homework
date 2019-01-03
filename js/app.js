fd97b5f619e05b4f11212013e897af68786a0110
Nicholas Humes <njhumes@gmail.com>
2019-01-02 21:29:55 -0700

    simpsons themed functional game except buttons

--- js/app.js
+++ js/app.js
@@ -12,33 +12,41 @@ class Pet {
     feed() {
         this.hunger -= 3;
         $('#hunger-level').text(`Hunger: ${this.hunger}`);
+        console.log(this.hunger)
+        $('.gif').remove()
+        $('.game-display').append('<img class="gif" src="https://i.pinimg.com/originals/b5/49/c5/b549c552687942777915f92519d5054c.gif">')
     }
     play() {
         this.boredom -= 4;
         $('#boredom-level').text(`Boredom: ${this.boredom}`);
+        $('.gif').remove()
+        $('.game-display').append('<img class="gif" src="https://i.gifer.com/origin/7d/7d6dd7aee61f0eae8caa330e54226a67_w200.gif">')
     }
     sleep() {
         this.sleepiness -= 4;
         $('#sleep-level').text(`Sleepiness: ${this.sleepiness}`);
+        $('.gif').remove()
+        $('.game-display').append('<img class="gif" src="https://i.pinimg.com/originals/20/7c/c5/207cc5f45d654c2b1f655b0bf1820bea.gif">')
     }
 }
 
-let ziggy = new Pet('Ziggy');
+let bart = new Pet('bart');
 
 
 function makeGame(){
-    $('.game-levels').append(`<p id="hunger-level">Hunger: ${ziggy.hunger}</p>`);
-    $('.game-levels').append(`<p id="boredom-level">Boredom: ${ziggy.boredom}</p>`);
-    $('.game-levels').append(`<p id="sleep-level">Sleepiness: ${ziggy.sleepiness}</p>`);
-    $('.game-levels').append(`<p id="age-level">Age: ${ziggy.age}</p>`);
+    $('.game-levels').append(`<p id="hunger-level">Hunger: ${bart.hunger}</p>`);
+    $('.game-levels').append(`<p id="boredom-level">Boredom: ${bart.boredom}</p>`);
+    $('.game-levels').append(`<p id="sleep-level">Sleepiness: ${bart.sleepiness}</p>`);
+    $('.game-levels').append(`<p id="age-level">Age: ${bart.age}</p>`);
     $('.game-buttons').append(`<button id="feed">Feed</button>`);
     $('.game-buttons').append(`<button id="play">Play</button>`);
     $('.game-buttons').append(`<button id="rest">Rest</button>`);
-    $('.game-display').append('<img class="gif" src="https://25.media.tumblr.com/a057dfb6d795792076571ae00f972506/tumblr_mmfwdqRCK11rcl9fxo1_500.gif">')
+    $('.game-display').append('<img class="bg" src="https://ih1.redbubble.net/image.455277575.6116/flat,550x550,075,f.jpg">')
+    $('.game-display').append('<img class="gif" src="https://i.gifer.com/origin/7d/7d6dd7aee61f0eae8caa330e54226a67_w200.gif">')
     $('.start-button').remove();
-    setInterval(increaseTime, 1000);
+    timeRunning;
 }
-
+let timeRunning = setInterval(increaseTime, 1000);
 // Start game when the button is clicked
 $('.start-button').click(makeGame);
 
@@ -46,24 +54,44 @@ $('.start-button').click(makeGame);
 function increaseTime(){
     game.time += 1;
     increaseLevels();
+    stopTimer();
+}
+function stopTimer() {
+    if(bart.hunger > 9){
+        clearInterval(timeRunning);
+        console.log('game over');
+    }
+    if(bart.boredom > 9){
+        clearInterval(timeRunning);
+        console.log('game over')
+    }
+    if (bart.sleepiness > 9) {
+        clearInterval(timeRunning);
+        console.log('game over')
+    }
 }
 
 function increaseLevels(){
     if(game.time % 2 === 0){
-        ziggy.hunger += 1;
-        $('#hunger-level').text(`Hunger: ${ziggy.hunger}`);
+        bart.hunger += 1;
+        $('#hunger-level').text(`Hunger: ${bart.hunger}`);
     }
     if(game.time % 3 === 0){
-        ziggy.boredom += 2;
-        $('#boredom-level').text(`Boredom: ${ziggy.boredom}`);
+        bart.boredom += 2;
+        $('#boredom-level').text(`Boredom: ${bart.boredom}`);
     }
     if(game.time % 5 === 0){
-        ziggy.sleepiness += 3;
-        $('#sleep-level').text(`Sleepiness: ${ziggy.sleepiness}`);
+        bart.sleepiness += 3;
+        $('#sleep-level').text(`Sleepiness: ${bart.sleepiness}`);
     }
     if(game.time % 15 === 0){
-        ziggy.age += 1;
-        $('#age-level').text(`Age: ${ziggy.age}`);
+        bart.age += 1;
+        $('#age-level').text(`Age: ${bart.age}`);
     }
 }
+
+// add functionality to game buttons
+$('.game').on('click', '#feed', bart.feed);
+$('.game').on('click', '#play', bart.play);
+$('.game').on('click', '#rest', bart.sleep);
 
